e22a1e79c6839f1e2e61691808a699f05d94ecce
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-01-04 18:32:41 +0100

    Update test with relative path

--- tests/test_xxx.py
+++ tests/test_xxx.py
@@ -1,6 +1,9 @@
 import pytest
+import os
 
 from tests.helpers.triple_store import TripleStore
+
+FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'files')
 
 def test_clear():
     ts = TripleStore()
@@ -8,4 +11,4 @@ def test_clear():
 
 def test_load():
     ts = TripleStore()
-    ts.upload("/Users/daveih/Documents/python/organizations/tests/files/org.ttl")
+    ts.upload(os.path.join(FIXTURE_DIR, "org.ttl"))
